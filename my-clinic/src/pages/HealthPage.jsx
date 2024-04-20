ad2632034b5cd872f4085b8759c172972de8ccc4
rcosta349 <rafael.costa2109@gmail.com>
2024-04-20 17:17:07 +0100

    .

--- my-clinic/src/pages/HealthPage.jsx
+++ my-clinic/src/pages/HealthPage.jsx
@@ -23,40 +23,40 @@ export default function HealthPage() {
       <Grid container spacing={2}>
         {options.map((option) => (
               
-        <Grid item xs={12} sm={4}>
-          <Item>
-          <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-start"
-              spacing={0}
-            >
-            <Avatar
-              src={BloodPressure}
-              sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 ,width: "200px", height: "200px"}} 
-              alt="Clinic Logo"
-            />
-            <Grid container  direction="column"
-              justifyContent="center"
-              alignItems="flex-start"
-              height="100%">
-              <Grid item >
-                <Typography variant="h4" gutterBottom>
-                  {option}
-                </Typography>
+          <Grid item xs={12} sm={4}>
+            <Item>
+            <Stack
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                spacing={0}
+              >
+              <Avatar
+                src={BloodPressure}
+                sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 ,width: "200px", height: "200px"}} 
+                alt="Clinic Logo"
+              />
+              <Grid container  direction="column"
+                justifyContent="center"
+                alignItems="flex-start"
+                height="100%">
+                <Grid item >
+                  <Typography variant="h4" gutterBottom>
+                    {option}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" gutterBottom>
+                    150
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography variant="h5" gutterBottom>
-                  150
-                </Typography>
-              </Grid>
-            </Grid>
+                
               
-            
-            </Stack>
-          </Item>
-        </Grid>
-      ))}
+              </Stack>
+            </Item>
+          </Grid>
+        ))}
         
 
       </Grid>
